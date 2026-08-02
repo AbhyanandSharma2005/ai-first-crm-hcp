66d16931f1923f1b39bdd471c1f66bafc5d5fea6
Abhyanand Sharma <abhyanandsharma827@gmail.com>
2026-08-02 23:43:04 +0530

    added

---
+++ frontend/src/components/LoadingTable.jsx
@@ -0,0 +1,51 @@
+import {
+
+    Box,
+
+    Skeleton
+
+} from "@mui/material";
+
+function LoadingTable({
+
+    rows = 6
+
+}) {
+
+    return (
+
+        <Box>
+
+            {
+
+                [...Array(rows)].map((_, index) => (
+
+                    <Skeleton
+
+                        key={index}
+
+                        variant="rounded"
+
+                        height={55}
+
+                        sx={{
+
+                            mb: 1,
+
+                            borderRadius: 2
+
+                        }}
+
+                    />
+
+                ))
+
+            }
+
+        </Box>
+
+    );
+
+}
+
+export default LoadingTable;
